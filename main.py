e25cc558e5f3b1748f71b5793791f10755e46cdc
mikwal-pl <mikolajwalczak98@gmail.com>
2025-10-26 15:37:16 +0100

    Added character counting

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import count_words
+from stats import count_characters
 
 def main():
     number_words = count_words("books/frankenstein.txt")
     print(f"Found {number_words} total words")
 
-main()
+def main2():
+    test = count_characters("books/frankenstein.txt")
+    print(test)
+
+main2()
 
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def count_words(filepath):
         file_contents = f.read()
         word_count = len(str.split(file_contents))
     return word_count
+
+def count_characters(filepath):
+    with open(filepath) as f:
+        char_dict = {}
+        file_cont = str.lower(f.read())
+        for char in file_cont:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+        return char_dict
+
